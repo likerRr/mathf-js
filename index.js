44df00c8a00263e81b6e1c6ed49d4e0f7e956533
Alexey Lizurchik <al.lizurchik@gmail.com>
2015-12-26 20:15:14 +0300

    initial

---
+++ index.js
@@ -0,0 +1,105 @@
+'use strict';
+
+class Mathf {
+  constructor() {
+    this.Deg2Rad = null;
+
+    this.Epsilon = null;
+
+    this.Infinity = null;
+
+    this.NegativeInfinity = null;
+
+    this.PI = null;
+
+    this.Rad2Deg = null;
+  }
+
+  static abs() {}
+
+  static acos() {}
+
+  static approximately() {}
+
+  static asin() {}
+
+  static atan() {}
+
+  static atan2() {}
+
+  static ceil() {}
+
+  static ceilToInt() {}
+
+  static clamp() {}
+
+  static clamp01() {}
+
+  static closestPowerOfTwo() {}
+
+  static cos() {}
+
+  static deltaAngle() {}
+
+  static exp() {}
+
+  static floor() {}
+
+  static floorToInt() {}
+
+  static gammaToLinearSpace() {}
+
+  static inverseLerp() {}
+
+  static isPowerOfTwo() {}
+
+  static lerp() {}
+
+  static lerpAngle() {}
+
+  static lerpUnclamped() {}
+
+  static linearToGammaSpace() {}
+
+  static log() {}
+
+  static log10() {}
+
+  static max() {}
+
+  static min() {}
+
+  static moveTowards() {}
+
+  static moveTowardsAngle() {}
+
+  static nextPowerOfTwo() {}
+
+  static perlinNoise() {}
+
+  static pingPong() {}
+
+  static pow() {}
+
+  static repeat() {}
+
+  static round() {}
+
+  static roundToInt() {}
+
+  static sign() {}
+
+  static sin() {}
+
+  static smoothDamp() {}
+
+  static smoothDampAngle() {}
+
+  static smoothStep() {}
+
+  static sqrt() {}
+
+  static tan() {}
+}
+
+module.exports = new Mathf();
