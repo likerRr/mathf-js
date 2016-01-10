f401c43fdb0db494ccabf1f28b3131b2a80e3618
Alexey Lizurchik <al.lizurchik@gmail.com>
2016-01-10 20:07:39 +0300

    improve performance: remove Math related methods and consts, wrap Mathf in class

--- index.js
+++ index.js
@@ -4,44 +4,22 @@ function toInt(value) {
   return value >> 0;
 }
 
-let Mathf = {
-  Deg2Rad: (Math.PI * 2) / 360,
 
-  Epsilon: Number.EPSILON || Math.pow(2, -52),
+class Mathf {
 
-  Infinity: Infinity,
+  static approximately(f1, f2) {
+    return Math.abs(f1 - f2) < Mathf.Epsilon;
+  }
 
-  NegativeInfinity: -Infinity,
-
-  PI: Math.PI,
-
-  Rad2Deg: 360 / (Math.PI * 2),
-
-  abs: Math.abs,
-
-  acos: Math.acos,
-
-  approximately: (f1, f2) => {
-    return Mathf.abs(f1 - f2) < Mathf.Epsilon;
-  },
-
-  asin: Math.asin,
-
-  atan: Math.atan,
-
-  atan2: Math.atan2,
-
-  ceil: Math.ceil,
-
-  clamp: (value, min, max) => {
+  static clamp(value, min, max) {
     return value < min ? min : (value > max ? max : value);
-  },
+  }
 
-  clamp01: (value) => {
+  static clamp01(value) {
     return value < 0 ? 0 : (value > 1 ? 1 : value);
-  },
+  }
 
-  closestPowerOfTwo: (value) => {
+  static closestPowerOfTwo(value) {
     let nextPowerOfTwo = Mathf.nextPowerOfTwo(value);
 
     if (nextPowerOfTwo - value > nextPowerOfTwo >> 2) {
@@ -49,9 +27,9 @@ let Mathf = {
     }
 
     return nextPowerOfTwo;
-  },
+  }
 
-  closestPowerOfTwoLong: (value) => {
+  static closestPowerOfTwoLong(value) {
     value = toInt(value);
 
     if (value < 0) return 0;
@@ -63,11 +41,9 @@ let Mathf = {
     }
 
     return nextPowerOfTwo;
-  },
+  }
 
-  cos: Math.cos,
-
-  deltaAngle: (current, target) => {
+  static deltaAngle(current, target) {
     if (Math.abs(current) > 360) {
       current %= 360;
     }
@@ -77,36 +53,32 @@ let Mathf = {
     }
 
     return target - current;
-  },
+  }
 
-  exp: Math.exp,
-
-  floor: Math.floor,
-
-  gammaToLinearSpace: (value) => {
+  static gammaToLinearSpace(value) {
     return Math.pow(value, 2.2);
-  },
+  }
 
-  inverseLerp: (min, max, value) => {
-    return (Mathf.clamp(value, Mathf.min(min, max), Mathf.max(min, max)) - min) / (max - min);
-  },
+  static inverseLerp(min, max, value) {
+    return (Mathf.clamp(value, Math.min(min, max), Math.max(min, max)) - min) / (max - min);
+  }
 
   /**
-   * @see http://stackoverflow.com/a/108360
-   * @param value
-   * @returns {boolean}
-   */
-  isPowerOfTwo: (value) => {
+  * @see http://stackoverflow.com/a/108360
+    * @param value
+  * @returns {boolean}
+  */
+  static isPowerOfTwo(value) {
     value = toInt(value);
 
     return (value & (value - 1)) === 0;
-  },
+  }
 
-  lerp: (a, b, t) => {
+  static lerp(a, b, t) {
     return (b - a) * Mathf.clamp01(t) + a;
-  },
+  }
 
-  lerpAngle: (a, b, t) => {
+  static lerpAngle(a, b, t) {
     while (a > b + 180) {
       b += 360;
     }
@@ -116,29 +88,21 @@ let Mathf = {
     }
 
     return Mathf.lerp(a, b, t);
-  },
+  }
 
-  lerpUnclamped: (a, b, t) => {
+  static lerpUnclamped(a, b, t) {
     if (t < 0 || t > 1) {
       return a + Math.abs(b - a) * t;
     }
 
     return (b - a) * Mathf.clamp01(t) + a;
-  },
+  }
 
-  linearToGammaSpace: (value) => {
+  static linearToGammaSpace(value) {
     return Math.pow(value, 0.45454545);
-  },
+  }
 
-  log: Math.log,
-
-  log10: Math.log10,
-
-  max: Math.max,
-
-  min: Math.min,
-
-  moveTowards: (current, target , maxDelta) => {
+  static moveTowards(current, target , maxDelta) {
     if (maxDelta > 0) {
       if (target < current && current - maxDelta < target) return target;
       else if (target > current && current + maxDelta > target) return target
@@ -149,11 +113,11 @@ let Mathf = {
     }
 
     return current + maxDelta;
-  },
+  }
 
-  moveTowardsAngle: () => {},
+  static moveTowardsAngle() {}
 
-  nextPowerOfTwo: (value) => {
+  static nextPowerOfTwo(value) {
     value = toInt(value);
 
     if (value < 0) return 0;
@@ -167,11 +131,11 @@ let Mathf = {
     value += 1;
 
     return value;
-  },
+  }
 
-  perlinNoise: () => {},
+  static perlinNoise() {}
 
-  pingPong: (t, length) => {
+  static pingPong(t, length) {
     if (t < 0) t = -t;
     var mod = t % length;
     // even
@@ -180,13 +144,11 @@ let Mathf = {
     }
 
     return (mod === 0) ? length : mod;
-  },
+  }
 
-  pow: Math.pow,
+  static repeat() {}
 
-  repeat: () => {},
-
-  round: (num) => {
+  static round(num) {
     let ceilVal = num + 0.5;
 
     if (ceilVal === Math.ceil(num)) {
@@ -194,24 +156,23 @@ let Mathf = {
     }
 
     return Math.round(num);
-  },
+  }
 
-  sign: (f) => {
+  static sign(f) {
     return (f >= 0) ? 1 : -1;
-  },
+  }
 
-  sin: Math.sin,
+  static smoothDamp() {}
 
-  smoothDamp: () => {},
+  static smoothDampAngle() {}
 
-  smoothDampAngle: () => {},
+  static smoothStep() {}
 
-  smoothStep: () => {},
+}
 
-  sqrt: Math.sqrt,
-
-  tan: Math.tan
-
-};
+Mathf.Deg2Rad = (Math.PI * 2) / 360;
+Mathf.Epsilon = Number.EPSILON || Math.pow(2, -52);
+Mathf.NegativeInfinity = -Infinity;
+Mathf.Rad2Deg = 360 / (Math.PI * 2);
 
 module.exports = Mathf;
