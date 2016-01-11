06a63d30207552b70ffd9910f8a17d07084413c8
Alexey Lizurchik <al.lizurchik@gmail.com>
2016-01-11 21:34:28 +0300

    add docs

--- index.js
+++ index.js
@@ -20,18 +20,49 @@ function toInt(value) {
 
 class Mathf {
 
+  /**
+   * Compares two floating point values if they are similar
+   *
+   * @see http://docs.unity3d.com/ScriptReference/Mathf.Approximately.html
+   * @param f1
+   * @param f2
+   * @returns {boolean}
+   */
   static approximately(f1, f2) {
     return Math.abs(f1 - f2) < Mathf.Epsilon;
   }
 
+  /**
+   * Clamps a value between a minimum float and maximum float value
+   *
+   * @see http://docs.unity3d.com/ScriptReference/Mathf.Clamp.html
+   * @param value
+   * @param min
+   * @param max
+   * @returns {number}
+   */
   static clamp(value, min, max) {
     return value < min ? min : (value > max ? max : value);
   }
 
+  /**
+   * Clamps value between 0 and 1 and returns value
+   *
+   * @see http://docs.unity3d.com/ScriptReference/Mathf.Clamp01.html
+   * @param value
+   * @returns {number}
+   */
   static clamp01(value) {
     return value < 0 ? 0 : (value > 1 ? 1 : value);
   }
 
+  /**
+   * Returns the closest power of two value
+   *
+   * @see http://docs.unity3d.com/ScriptReference/Mathf.ClosestPowerOfTwo.html
+   * @param value
+   * @returns {number}
+   */
   static closestPowerOfTwo(value) {
     let nextPowerOfTwo = Mathf.nextPowerOfTwo(value);
 
@@ -44,6 +75,13 @@ class Mathf {
     return nextPowerOfTwo;
   }
 
+  /**
+   * Returns the closest power of two long value (useful for more then 32 bit numbers)
+   *
+   * @see http://docs.unity3d.com/ScriptReference/Mathf.ClosestPowerOfTwo.html
+   * @param value
+   * @returns {number}
+   */
   static closestPowerOfTwoLong(value) {
     value = toInt(value);
 
@@ -61,6 +99,14 @@ class Mathf {
     return nextPowerOfTwo;
   }
 
+  /**
+   * Calculates the shortest difference between two given angles given in degrees
+   *
+   * @see http://docs.unity3d.com/ScriptReference/Mathf.DeltaAngle.html
+   * @param current
+   * @param target
+   * @returns {number}
+   */
   static deltaAngle(current, target) {
     if (Math.abs(current) > FULL_ANGLE) {
       current %= FULL_ANGLE;
@@ -73,29 +119,66 @@ class Mathf {
     return target - current;
   }
 
+  /**
+   * Converts the given value from gamma (sRGB) to linear color space
+   *
+   * @see http://docs.unity3d.com/ScriptReference/Mathf.GammaToLinearSpace.html
+   * @param value
+   * @returns {number}
+   */
   static gammaToLinearSpace(value) {
     return Math.pow(value, GAMMA_TO_LINEAR);
   }
 
-  static inverseLerp(min, max, value) {
-    return (Mathf.clamp(value, Math.min(min, max), Math.max(min, max)) - min) / (max - min);
+  /**
+   * Calculates the linear parameter t that produces the interpolant value within the range [a, b]
+   *
+   * @see http://docs.unity3d.com/ScriptReference/Mathf.InverseLerp.html
+   * @param a
+   * @param b
+   * @param value
+   * @returns {number}
+   */
+  static inverseLerp(a, b, value) {
+    return (Mathf.clamp(value, Math.min(a, b), Math.max(a, b)) - a) / (b - a);
   }
 
   /**
-  * @see http://stackoverflow.com/a/108360
-    * @param value
-  * @returns {boolean}
-  */
+   * Returns true if the value is power of two
+   *
+   * @see http://docs.unity3d.com/ScriptReference/Mathf.IsPowerOfTwo.html
+   * @link http://stackoverflow.com/a/108360
+   * @param value
+   * @returns {boolean}
+   */
   static isPowerOfTwo(value) {
     value = toInt(value);
 
     return (value & (value - 1)) === 0;
   }
 
+  /**
+   * Linearly interpolates between a and b by t
+   *
+   * @see http://docs.unity3d.com/ScriptReference/Mathf.Lerp.html
+   * @param a
+   * @param b
+   * @param t
+   * @returns {number}
+   */
   static lerp(a, b, t) {
     return (b - a) * Mathf.clamp01(t) + a;
   }
 
+  /**
+   * Same as Lerp but makes sure the values interpolate correctly when they wrap around 360 degrees
+   *
+   * @see http://docs.unity3d.com/ScriptReference/Mathf.LerpAngle.html
+   * @param a
+   * @param b
+   * @param t
+   * @returns {number}
+   */
   static lerpAngle(a, b, t) {
     while (a > b + STRAIGHT_ANGLE) {
       b += FULL_ANGLE;
@@ -108,6 +191,15 @@ class Mathf {
     return Mathf.lerp(a, b, t);
   }
 
+  /**
+   * Linearly interpolates between a and b by t
+   *
+   * @see http://docs.unity3d.com/ScriptReference/Mathf.LerpUnclamped.html
+   * @param a
+   * @param b
+   * @param t
+   * @returns {number}
+   */
   static lerpUnclamped(a, b, t) {
     if (t < 0 || t > 1) {
       return a + Math.abs(b - a) * t;
@@ -116,10 +208,26 @@ class Mathf {
     return (b - a) * Mathf.clamp01(t) + a;
   }
 
+  /**
+   * Converts the given value from linear to gamma (sRGB) color space
+   *
+   * @see http://docs.unity3d.com/ScriptReference/Mathf.LinearToGammaSpace.html
+   * @param value
+   * @returns {number}
+   */
   static linearToGammaSpace(value) {
     return Math.pow(value, LINEAR_TO_GAMMA);
   }
 
+  /**
+   * Moves a value current towards target
+   *
+   * @see http://docs.unity3d.com/ScriptReference/Mathf.MoveTowards.html
+   * @param current
+   * @param target
+   * @param maxDelta
+   * @returns {number}
+   */
   static moveTowards(current, target , maxDelta) {
     if (maxDelta > 0) {
       if (target < current && current - maxDelta < target) return target;
@@ -133,8 +241,26 @@ class Mathf {
     return current + maxDelta;
   }
 
-  static moveTowardsAngle() {}
+  /**
+   * TODO: need help in reverse engineering
+   * Same as MoveTowards but makes sure the values interpolate correctly when they wrap around 360 degrees
+   *
+   * @see http://docs.unity3d.com/ScriptReference/Mathf.MoveTowardsAngle.html
+   * @deprecated
+   * @param current
+   * @param target
+   * @param maxDelta
+   * @returns {number}
+   */
+  static moveTowardsAngle(current, target, maxDelta) {}
 
+  /**
+   * Returns the next power of two value
+   *
+   * @see http://docs.unity3d.com/ScriptReference/Mathf.NextPowerOfTwo.html
+   * @param value
+   * @returns {number}
+   */
   static nextPowerOfTwo(value) {
     value = toInt(value);
 
@@ -151,10 +277,26 @@ class Mathf {
     return value;
   }
 
+  /**
+   * Generate 2D Perlin noise
+   *
+   * @see http://docs.unity3d.com/ScriptReference/Mathf.PerlinNoise.html
+   * @param x
+   * @param y
+   * @returns {number}
+   */
   static perlinNoise(x, y) {
     return (new SimplexNoise(RANDOM_SEED)).noise(x, y);
   }
 
+  /**
+   * PingPongs the value t, so that it is never larger than length and never smaller than 0
+   *
+   * @see http://docs.unity3d.com/ScriptReference/Mathf.PingPong.html
+   * @param t
+   * @param length
+   * @returns {number}
+   */
   static pingPong(t, length) {
     if (t < 0) t = -t;
     var mod = t % length;
@@ -166,31 +308,92 @@ class Mathf {
     return (mod === 0) ? length : mod;
   }
 
+  /**
+   * Loops the value t, so that it is never larger than length and never smaller than 0
+   *
+   * @see http://docs.unity3d.com/ScriptReference/Mathf.Repeat.html
+   * @param t
+   * @param length
+   * @returns {number}
+   */
   static repeat(t, length) {
     if (t > 0) return t % length;
 
     return length + (t % length);
   }
 
-  static round(num) {
-    let ceilVal = num + IS_INTEGER;
+  /**
+   * Returns f rounded to the nearest integer
+   *
+   * @see http://docs.unity3d.com/ScriptReference/Mathf.Round.html
+   * @param f
+   * @returns {number}
+   */
+  static round(f) {
+    let ceilVal = f + IS_INTEGER;
 
-    if (ceilVal === Math.ceil(num)) {
-      return (ceilVal % 2 === 0) ? num + IS_INTEGER : num - IS_INTEGER;
+    if (ceilVal === Math.ceil(f)) {
+      return (ceilVal % 2 === 0) ? f + IS_INTEGER : f - IS_INTEGER;
     }
 
-    return Math.round(num);
+    return Math.round(f);
   }
 
+  /**
+   * Returns the sign of f
+   *
+   * @see http://docs.unity3d.com/ScriptReference/Mathf.Sign.html
+   * @param f
+   * @returns {number}
+   */
   static sign(f) {
     return (f >= 0) ? 1 : -1;
   }
 
-  static smoothDamp() {}
+  /**
+   * TODO: need help in reverse engineering
+   * Gradually changes a value towards a desired goal over time
+   *
+   * @see http://docs.unity3d.com/ScriptReference/Mathf.SmoothDamp.html
+   * @deprecated
+   * @param current
+   * @param target
+   * @param currentVelocity
+   * @param smoothTime
+   * @param maxSpeed
+   * @param deltaTime
+   * @returns {number}
+   */
+  static smoothDamp(current, target, currentVelocity, smoothTime, maxSpeed, deltaTime) {}
 
-  static smoothDampAngle() {}
+  /**
+   * TODO: need help in reverse engineering
+   * Gradually changes an angle given in degrees towards a desired goal angle over time
+   *
+   * @see http://docs.unity3d.com/ScriptReference/Mathf.SmoothDampAngle.html
+   * @deprecated
+   * @param current
+   * @param target
+   * @param currentVelocity
+   * @param smoothTime
+   * @param maxSpeed
+   * @param deltaTime
+   * @returns {number}
+   */
+  static smoothDampAngle(current, target, currentVelocity, smoothTime, maxSpeed, deltaTime) {}
 
-  static smoothStep() {}
+  /**
+   * TODO: need to find proper easing algorithm
+   * Interpolates between min and max with smoothing at the limits
+   *
+   * @see http://docs.unity3d.com/ScriptReference/Mathf.SmoothStep.html
+   * @deprecated
+   * @param a
+   * @param b
+   * @param t
+   * @returns {number}
+   */
+  static smoothStep(a, b, t) {}
 
 }
 
